b788c1dbc824930064028abc7113ebf1359ca5be
oshLunch <zldtmskdlxm@gmail.com>
2023-10-06 14:47:54 +0900

    전체적 디자인 수정

--- src/tour/tourItem.js
+++ src/tour/tourItem.js
@@ -2,29 +2,28 @@
 
 const TourItem = ({article}) => {
     const {MAIN_TITLE,      /* 콘텐츠 이름 */
-           TITLE,           /* 제목 */
+           SUBTITLE,        /* 부제목 */
            GUGUN_NM,        /* 구군 */
-           HOMEPAGE_URL,    /* 홈페이지 */
+           ADDR1,           /* 주소 */
+           CNTCT_TEL,       /* 연락처 */
            MAIN_IMG_NORMAL  /* 이미지 URL */
     } = article;
 
     return (
         <div className = 'item'>
             <div className = 'title'>
-                <div className="name">
-                    <strong>{MAIN_TITLE}</strong>
-                    <span>{GUGUN_NM}</span>
-                </div>
-
-                <div className="info">
-                    <h4>{TITLE}</h4>
-                    {HOMEPAGE_URL && <a href={HOMEPAGE_URL} target='_blank' rel="noreferrer">더 알아보기 ▶</a>}
-                </div>
+                <h4>{SUBTITLE}</h4>
+                <strong><span>{GUGUN_NM}</span> {MAIN_TITLE}</strong>
             </div> {/* title end */}
 
             <figure>
-                <img src={MAIN_IMG_NORMAL} alt={TITLE} />
+                <img src={MAIN_IMG_NORMAL} alt={SUBTITLE} />
             </figure>
+
+            <div className="info">
+                <span>{ADDR1}</span>
+                <span>{CNTCT_TEL}</span>
+            </div>
             {/* noreferrer : 해당 페이지로 넘어가기 위해 http에게 요청함 */}
         </div> /* item end */
     )
